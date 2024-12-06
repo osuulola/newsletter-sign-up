cd22a9877b1a812959ff893b1c289e72612c6584
Oludare Suulola <osuulola@yahoo.com>
2024-12-06 07:27:49 +0000

    commit

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 const sub = document.getElementById('sub');
+const con = document.getElementById('container');
+const card = document.getElementById('card-sub');
+
+sub.addEventListener('click', handleClick)
+
+function handleClick(e){
+    e.preventDefault(); 
+    con.classList.add('card-sub');
+    card.style.display = 'block';
+    
+    console.log(sub);
+}
